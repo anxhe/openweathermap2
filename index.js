589f22a5d71716fb4eb8e4ddf8cbb8c2dd589ce4
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2018-01-18 13:02:15 +0100

    Add map, markers, infoWindows and remove unwanted character

--- index.js
+++ index.js
@@ -13,43 +13,70 @@ const ANDALUCIA = [
 
 const APPID = "appid=a3a348e0caa4a617475b716bb95e9b8f"
 const UNITS = "units=metric"
+const andaluciaCoord = {lat: 37.4, lng: -4.48}
+
+window.onload = () => {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: andaluciaCoord
+  });
+}
 
 function newRequest(URL){
-	//New Promise recibe una funcion
+  //New Promise recibe una funcion
   return new Promise((resolve, reject)=> {
-		const req = new XMLHttpRequest();
-		req.onreadystatechange  = (aEvt) => {
-			const DONE = 4
-			const OK = 200
-			if (req.readyState === DONE) {
-				if(req.status === OK){
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = (e) => {
+      if (req.readyState === 4) {
+        if(req.status === 200){
           resolve(JSON.parse(req.responseText))
-				} else {
-					reject(new Error(`Se produjo un error: ${req.status}`))
-				}
-			}
-		}
-		req.open('GET', URL);
-		req.send(null);
-	})
+        } else {
+          reject(new Error(`Se produjo un error: ${req.status}`))
+        }
+      }
+    }
+    req.open('GET', URL);
+    req.send(null);
+  })
 }
 
 /* Funcion para tratar el error */
 function handleError(err){
-	console.log(`Request failed: ${err}`)
+  console.error(`Request failed: ${err}`)
 }
 
-/*Crear un array con el request de cada provincia*/
+/* Function para dibujar */
+function draw(lat, lng, temp){
+  const marker = new google.maps.Marker({
+    position: {lat, lng},
+    map: map,
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: `<span>Temperatura: ${temp}</span>`
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+/* Crear un array con el request de cada provincia*/
 const requests = ANDALUCIA.map((province) => newRequest(`${BASE}${province}&${APPID}&${UNITS}`));
 
-/*Cuando esten resueltos todos los requests, acumulamos la temperatura de cada provincia y calculamos la media*/
-
+/* Cuando esten resueltos todos los requests, acumulamos la temperatura de cada provincia y calculamos la media*/
 Promise.all(requests)
   .then(provinces => {
-    let average = 0
+    let accum = 0
     provinces.forEach((province) => {
-      average += province.main.temp
+      let temp = province.main.temp;
+      let lon = province.coord.lon;
+      let lat = province.coord.lat;
+      accum += temp
+      draw(lat, lon, temp);
     })
-    console.log(average/provinces.length);
+    const infowindow = new google.maps.InfoWindow({
+      content: `<span>Average temp: ${accum/provinces.length}</span>`,
+      position: andaluciaCoord
+    });
+    infowindow.open(map, map);
   })
   .catch(err => handleError(err))
